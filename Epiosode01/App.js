98f5c91b352b329c9b78e1d92cf611e118f0f40d
sourabh.suryavanshi <sourabh.suryavanshi@laminaar.in>
2024-04-12 18:52:59 +0530

    Episode2 commit

--- Epiosode01/App.js
+++ Epiosode01/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const parent = React.createElement("div",{id:"parent"},
                        [ React.createElement("div",{id:"child1"},
@@ -9,4 +11,5 @@ const parent = React.createElement("div",{id:"parent"},
 console.log(parent);
 //const heading = React.createElement("h1",{id:"heading"},"Hello Word From React");
 const root = ReactDOM.createRoot(document.getElementById("root"))
+console.log(root);
 root.render(parent);
