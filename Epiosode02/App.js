77c1184d425abc46e378713583304bbb63c46407
Sourabh-Suryavanshi <110523147+Sourabh-Suryavanshi@users.noreply.github.com>
2024-04-25 18:53:45 +0530

    Update App.js

--- Epiosode02/App.js
+++ Epiosode02/App.js
@@ -3,7 +3,29 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", { id: "heading" }, "Hello Laminaar");
 
-const jsxHeading = <h1 id="heading">Hello Laminaar from JSX</h1>
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hello React from JSX
+  </h1>
+);
+
+const Headingcomponent = () => (
+  <div id="container">
+    <h1 id="head">Namaste React !</h1>
+    <Headingcomponent1 />
+  </div>
+);
+
+const Headingcomponent1 = () => {
+  return (
+    <div>
+      <h1> Hello I am React Functional Component!</h1>
+      <Headingcomponent2 />
+    </div>
+  );
+};
+
+const Headingcomponent2 = () => <h1> Hello I am React Functional child !</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Headingcomponent />);
