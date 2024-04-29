a7cc1b0307ada2372a7a17a8d92393514757322a
sourabh.suryavanshi <sourabh.suryavanshi@laminaar.in>
2024-04-29 12:49:00 +0530

    UseEffect, ShimmerUI

--- Episode4_FoodPlaza/src/components/Body.js
+++ Episode4_FoodPlaza/src/components/Body.js
@@ -1,12 +1,50 @@
-import RestaurantCard from "./RestaurantCard"
-import resList from "../utils/mockData"
+import RestaurantCard from "./RestaurantCard";
+//import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {/* <RestaurantCard
+  const [listofRestaurants, setListofrestaurants] = useState([]);
+  // const arr = useState(resList);
+  // const listofRestaurants = arr[0];
+  // const setListofrestaurants = arr[1];
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const URL =
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const data = await fetch(URL);
+    const json = await data.json();
+
+    setListofrestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listofRestaurants.length == 0) {
+    return <Shimmer />;
+  }
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdata = listofRestaurants.filter(
+              (res) => res?.info?.avgRating > 4
+            );
+            setListofrestaurants(filterdata);
+          }}
+        >
+          TOP RATED RESTAURANTS
+        </button>
+      </div>
+      <div className="res-container">
+        {/* <RestaurantCard
             resName="Hotel Swagat"
             cuisine="Biryani, North Indian, Asian"
             rating="4.4 Star"
@@ -18,11 +56,11 @@ const Body = () => {
             rating="4.3 Star"
             ETA="15 min"
           /> */}
-          {resList.map((restaurant) => (
-            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
-          ))}
-        </div>
+        {listofRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+        ))}
       </div>
-    );
-  };
-  export default Body;
+    </div>
+  );
+};
+export default Body;
---
+++ Episode4_FoodPlaza/src/components/Shimmer.js
@@ -0,0 +1,25 @@
+const Shimmer = () => {
+  console.log("Shimmer Called");
+  return (
+    <div>
+      <div className="btn">
+        <button className="Sfilter-btn"></button>
+      </div>
+      <div className="shimmer-container">
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
