222c5f2b613bd0a66a447c9275ed5875c5c6333f
sourabh.suryavanshi <sourabh.suryavanshi@laminaar.in>
2024-05-02 16:12:50 +0530

    Code Uploaded

--- Episode4_FoodPlaza/src/components/About.js
+++ Episode4_FoodPlaza/src/components/About.js
@@ -3,21 +3,26 @@ import Userclass from "./Userclass";
 import Users from "./Users";
 
 const About = () => {
-  const [count, setCount] = useState(5)
-  const [disable,setDisable] = useState(false)
+  const [count, setCount] = useState(5);
+  const [disable, setDisable] = useState(false);
 
-  const IncrementCount =()=>{
-      if(count < 10)setCount(count+1)
-      if(count == 10) setDisable(true)
-  }
+  const IncrementCount = () => {
+    if (count < 10) setCount(count + 1);
+    if (count == 10) setDisable(true);
+  };
   return (
     <div>
       <h1>About Us</h1>
       <h2>Count : {count}</h2>
       <h2>This webpage is designed by Mr. Sourabh Sambhaji Suryavanshi</h2>
       <div className="User-Component">
-      <Users name={"Sourabh"} location={"Sangli"} IncrementCount={IncrementCount} button={disable}/>
-      <Userclass name={"Manish"} location={"Ahamadnagar"}/>
+        {/* <Users
+          name={"Sourabh"}
+          location={"Sangli"}
+          IncrementCount={IncrementCount}
+          button={disable}
+        /> */}
+        <Userclass name={"Manish"} location={"Ahamadnagar"} />
       </div>
     </div>
   );
--- Episode4_FoodPlaza/src/components/Userclass.js
+++ Episode4_FoodPlaza/src/components/Userclass.js
@@ -1,32 +1,38 @@
 import React from "react";
-class Userclass extends React.Component{
-   constructor(props){
-    super(props)
-    this.state={
-      count:0,
-      count1:1
-    }
-   }
-   render(){
-      const {name,location} = this.props
-      const {count,count1 } = this.state
+class Userclass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count1: 1,
+    };
+    console.log("Constructor Called");
+  }
 
-      return (
+  componentDidMount() {
+    console.log("Component Did mount");
+  }
+  render() {
+    console.log("Render");
+    const { name, location } = this.props;
+    const { count, count1 } = this.state;
+
+    return (
       <div className="user-card">
-       <h1>Count : {count}</h1>  
-       <button onClick={()=>
-          {this.setState(
-            {count:this.state.count +1}
-            )
-            }
-            }>Count Increaser</button>
-       {/* <h1>Count : {count1}</h1>   */}
-      <h1>Name : {name}</h1>
-      <h3>Location : {location}</h3>
-      <p>Contact : sourabhss9018@gmail.com
-      </p>
-  </div>
-    )
-   }
+        <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count Increaser
+        </button>
+        {/* <h1>Count : {count1}</h1>   */}
+        <h1>Name : {name}</h1>
+        <h3>Location : {location}</h3>
+        <p>Contact : sourabhss9018@gmail.com</p>
+      </div>
+    );
+  }
 }
 export default Userclass;
